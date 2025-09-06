1e78413f858ceb55ff74af1050d9e9612f2a62b1
udayadevi <150006560+udayadevi@users.noreply.github.com>
2025-09-06 23:34:58 +0530

    Update sort array.py

--- sort array.py
+++ sort array.py
@@ -6,3 +6,17 @@ for i in range(1,len(nums)):
         break
 else:
     print("True")
+#Time complexity is o(n)
+#Space complexity is o(1)
+
+
+
+
+#like for another condition
+a=[1,2,3,4]
+for i in range(1,len(nums)):
+    if a[i]<a[i-1]:
+        print("False")
+        break
+else:
+    print("True")
