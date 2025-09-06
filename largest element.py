4a0715514e20a7270f76d4d25d8137b10db3a3f3
udayadevi <150006560+udayadevi@users.noreply.github.com>
2025-09-06 23:34:21 +0530

    Update largest element.py

--- largest element.py
+++ largest element.py
@@ -3,6 +3,8 @@
 nums=[1,2,3,8,4,9]
 nums.sort()
 print(nums[-1])
+#Time compolexity o(n log n)
+#Space complexity o(n)
 #Optimized approach
 a=[1,8,3,4,7,5,6]
 max=a[0]
@@ -10,4 +12,7 @@ for i in range(len(a)):
     if a[i]>max:
         max=a[i]
 print(max)
+#Time complexity is o(n)
+#space complexity is o(1)
+
 
